8493eafa9a015ec610bbd993846fd6db4a5d1eca
Alê Borba <alexandre.borba@imasters.com.br>
2012-07-12 20:37:00 -0300

    Ajuste no setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ from distutils.core import setup
 setup(name='Moipy',
       version='0.2',
       description='Python integration with MoIP payment gateway via API',
-      author='Herberth Amaral',
-      author='Ale Borba',
-      author_email='herberthamaral@gmail.com',
-      author_email='ale.alvesborba@gmail.com',
+      author=['Herberth Amaral','Ale Borba'],
+      author_email=['herberthamaral@gmail.com','ale.alvesborba@gmail.com'],
       url='http://labs.moip.com.br/',
       packages=['moipy'],
       classifiers=[
